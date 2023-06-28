b061304219879a889b2152ac7494f694972764fb
acampetella <andrea.campetella@libero.it>
2023-06-28 19:48:07 +0200

    Cook candidate registration completed

--- src/pages/CookCandidateRegistration.jsx
+++ src/pages/CookCandidateRegistration.jsx
@@ -1,72 +1,129 @@
-import React from 'react';
-import Logo from '../components/Logo';
-import { useState } from 'react';
-import { getFormDataValidation } from '../utilities/validations/formDataValidation';
-import { Toaster } from 'react-hot-toast';
-import { Toast } from '../utilities/notifications/toast';
+import React from "react";
+import Logo from "../components/Logo";
+import { useState } from "react";
+import { getFormDataValidation } from "../utilities/validations/formDataValidation";
+import { Toaster } from "react-hot-toast";
+import { Toast } from "../utilities/notifications/toast";
+import { getFormatFileValidation } from "../utilities/validations/formatFileValidation";
+import Loader from "../components/loaders/Loader";
 
 const CookCandidateRegistration = () => {
-
   const formDataInitialValue = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    curriculum: ''
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    resume: "",
   };
 
-  const valuesTypes = ['string', 'string', 'string', 'string', 'string'];
+  const valuesTypes = ["string", "string", "string", "string", "string"];
+  const allowedFileFormats = ["pdf"];
 
   const [formData, setFormData] = useState(formDataInitialValue);
+  const [file, setFile] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (event) => {
-    setFormData(prev => ({ ...prev, [event.target.name]: event.target.value }))
+    setFormData((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleFileChange = (event) => {
+    handleChange(event);
+    setFile(event.target.files[0]);
   };
 
   const checkFormData = () => {
     return getFormDataValidation(formData, valuesTypes);
   };
 
+  const checkFileFormat = () => {
+    return getFormatFileValidation(formData.resume, allowedFileFormats);
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     let myToast;
     const result = checkFormData();
+    const checkFormat = checkFileFormat();
     if (result === true) {
-      //qui va fatta la fetch
-      try {
-        const data = await fetch('http://localhost:5050/cooksCandidates', {
-          method: 'POST',
-          body: JSON.stringify(formData),
-          headers: {
-            "Content-Type": "application/json",
+      if (checkFormat === true) {
+        //qui va fatta la fetch
+        setIsLoading(true);
+        try {
+          const uploadedFile = await resumeUpload(
+            file,
+            `${process.env.REACT_APP_SERVER_BASE_URL}/resumeUpload`
+          );
+          if (uploadedFile !== undefined) {
+            const newFormData = {
+              ...formData,
+              resume: uploadedFile.resume
+            };
+            const data = await fetch(
+              `${process.env.REACT_APP_SERVER_BASE_URL}/cooksCandidates`,
+              {
+                method: "POST",
+                body: JSON.stringify(newFormData),
+                headers: {
+                  "Content-Type": "application/json"
+                }
+              }
+            );
+            const response = await data.json();
+            if (response.statusCode === 201) {
+              setIsLoading(false);
+              myToast = new Toast("Operation successfully completed");
+              myToast.notifyMessage();
+              setFormData(formDataInitialValue);
+            } else {
+              const errorMessage = `statusCode: ${response.statusCode}, message: ${response.message}`;
+              myToast = new Toast(errorMessage);
+              myToast.notifyError();
+            }
+          } else {
+            myToast = new Toast("Operation failed: upload file error");
+            myToast.notifyError();
           }
-        });
-        const response = await data.json();
-        if (response.statusCode === 201) {
-          myToast = new Toast('Operation successfully completed');
-          myToast.notifyMessage();
-          setFormData(formDataInitialValue);
-        } else {
-          const errorMessage = `statusCode: ${response.statusCode}, message: ${response.message}`;
-          myToast = new Toast(errorMessage);
-          myToast.notifyError();
-        }
-      } catch (error) {
+          setIsLoading(false);
+        } catch (error) {
           myToast = new Toast(error.toString());
           myToast.notifyError();
+        }
+      } else {
+        const errorMessage = `${checkFormat}. Allowed format: ${[...allowedFileFormats]}`;
+        myToast = new Toast(errorMessage);
+        myToast.notifyError();
       }
     } else {
       myToast = new Toast(result);
       myToast.notifyError();
     }
-  }
+  };
+
+  const resumeUpload = async (file, endpoint) => {
+    const fileData = new FormData();
+    fileData.append("resume", file);
+
+    try {
+      const data = await fetch(endpoint, {
+        method: "POST",
+        body: fileData,
+      });
+      const response = await data.json();
+      return response;
+    } catch (error) {
+      const myToast = new Toast(error.toString());
+      myToast.notifyError();
+    }
+  };
 
   return (
     <div className="font-mono bg-green-800 w-screen h-screen flex items-center">
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-      />
+      <Toaster position="top-center" reverseOrder={false} />
+      {isLoading && <Loader/>}
       <div className="container mx-auto">
         <div className="flex justify-center px-6 my-12">
           <div className="w-full xl:w-3/4 lg:w-11/12 flex">
@@ -76,8 +133,14 @@ const CookCandidateRegistration = () => {
               className="w-full h-auto bg-gray-400 hidden lg:block lg:w-5/12 bg-cover rounded-l-lg"
             />
             <div className="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
-              <h3 className="pt-4 text-2xl text-center">Invia la tua candidatura</h3>
-              <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded" onSubmit={handleSubmit}>
+              <h3 className="pt-4 text-2xl text-center">
+                Invia la tua candidatura
+              </h3>
+              <form
+                className="px-8 pt-6 pb-8 mb-4 bg-white rounded"
+                onSubmit={handleSubmit}
+                encType="multipart/form-data"
+              >
                 <div className="mb-4 lg:flex lg:justify-between">
                   <div className="mb-4 lg:mr-2 lg:mb-0">
                     <label className="block mb-2 text-sm font-bold text-gray-700">
@@ -87,7 +150,7 @@ const CookCandidateRegistration = () => {
                       className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                       type="text"
                       placeholder="Nome"
-                      name='firstName'
+                      name="firstName"
                       onChange={handleChange}
                       value={formData.firstName}
                     />
@@ -100,14 +163,14 @@ const CookCandidateRegistration = () => {
                       className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                       type="text"
                       placeholder="Cognome"
-                      name='lastName'
+                      name="lastName"
                       onChange={handleChange}
                       value={formData.lastName}
                     />
                   </div>
                 </div>
                 <div className="mb-4 lg:flex lg:justify-between">
-                  <div className="lg:ml-2">
+                  <div className="mb-4 lg:mr-2 lg:mb-0">
                     <label className="block mb-2 text-sm font-bold text-gray-700">
                       Email
                     </label>
@@ -115,20 +178,20 @@ const CookCandidateRegistration = () => {
                       className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                       type="email"
                       placeholder="Indirizzo email"
-                      name='email'
+                      name="email"
                       onChange={handleChange}
                       value={formData.email}
                     />
                   </div>
-                  <div className="mb-4 lg:mr-2 lg:mb-0">
+                  <div className="lg:ml-2">
                     <label className="block mb-2 text-sm font-bold text-gray-700">
                       Telefono
                     </label>
                     <input
                       className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                       type="text"
-                      placeholder='Telefono'
-                      name='phone'
+                      placeholder="Telefono"
+                      name="phone"
                       onChange={handleChange}
                       value={formData.phone}
                     />
@@ -142,9 +205,8 @@ const CookCandidateRegistration = () => {
                     <input
                       className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                       type="file"
-                      name='curriculum'
-                      onChange={handleChange}
-                      value={formData.curriculum}
+                      name="resume"
+                      onChange={handleFileChange}
                     />
                   </div>
                 </div>
@@ -173,7 +235,7 @@ const CookCandidateRegistration = () => {
                   </a>
                 </div>
                 <div className="flex justify-center mt-5">
-                  <Logo/>
+                  <Logo />
                 </div>
               </form>
             </div>
@@ -181,7 +243,7 @@ const CookCandidateRegistration = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default CookCandidateRegistration;
