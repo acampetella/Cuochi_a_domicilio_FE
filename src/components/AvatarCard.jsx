6a2e54ea51f6601443ef7d06be9d3aa4423a9c9b
acampetella <andrea.campetella@libero.it>
2023-07-03 21:05:55 +0200

    add tooltip text to AvatarCard

--- src/components/AvatarCard.jsx
+++ src/components/AvatarCard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectedAvatarURL, setSelectedAvatarURL } from '../reducers/buttonSelectedAvatarReducer';
 import { useRef } from 'react';
+import "../styles/avatarCardStyle.css";
 
 const AvatarCard = ({}) => {
 
@@ -23,7 +24,7 @@ const AvatarCard = ({}) => {
 
   return (
     <button 
-      className='bg-slate-200 border-4 border-slate-300 shadow-lg rounded-full absolute bottom-[-20px] left-8 md:bottom-[-40px] md:left-10 xl:bottom-[-50px] xl:left-20 w-[50px] h-[50px] md:w-[100px] md:h-[100px] lg:w-[150px] lg:h-[150px] xl:w-[200px] xl:h-[200px]'
+      className='tooltip bg-slate-200 border-4 border-slate-300 shadow-lg rounded-full absolute bottom-[-20px] left-8 md:bottom-[-40px] md:left-10 xl:bottom-[-50px] xl:left-20 w-[50px] h-[50px] md:w-[100px] md:h-[100px] lg:w-[150px] lg:h-[150px] xl:w-[200px] xl:h-[200px]'
       onClick={handleClick}
     >
         { url && 
@@ -39,6 +40,7 @@ const AvatarCard = ({}) => {
             accept="image/png, image/jpeg"
             className='hidden' 
             />
+            <span className="tooltiptext hidden w-[50px] md:w-[100px] lg:w-[150px] xl:w-[200px] py-1 md:py-2 xl:py-3 bg-purple-200 text-black text-center rounded-md absolute z-10 bottom-[-20px] left-8 md:bottom-[-40px] md:left-10 xl:bottom-[-50px] xl:left-20">Carica nuova immagine</span>
     </button>
   )
 }
